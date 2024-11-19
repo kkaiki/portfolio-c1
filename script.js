1f7e0b683e2c7a140e181df0f14f1ee0a1798d65
kaiki <kanokaiki@gmail.com>
2024-11-18 19:46:56 -0800

    refactor: a bit of change

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Load header and footer
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-placeholder').innerHTML = data;
-        });
+// document.addEventListener('DOMContentLoaded', function() {
+//     fetch('header.html')
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById('header-placeholder').innerHTML = data;
+//         });
 
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-placeholder').innerHTML = data;
-        });
-});
+//     fetch('footer.html')
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById('footer-placeholder').innerHTML = data;
+//         });
+// });
 
 // Navigation
 document.addEventListener('click', function(e) {
